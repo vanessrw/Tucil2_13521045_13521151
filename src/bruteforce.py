a9ca838c39e3e4c6e672cd6095cbf05318ed49e1
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-02-27 17:06:03 +0700

    multiple dots

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -1,5 +1,7 @@
 import math
 import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def jarak(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
@@ -9,14 +11,19 @@ def closest_points(points):
     min_dist = float('inf')
     closest_p1, closest_p2 = None, None
     
+    arrPoints1 = []
+    arrPoints2 = []
     for i in range(n):
         for j in range(i+1, n):
             dist = jarak(points[i], points[j])
             if dist < min_dist:
                 min_dist = dist
                 closest_p1, closest_p2 = points[i], points[j]
+                arrPoints1.append(closest_p1)
+                arrPoints2.append(closest_p2)
+
     
-    return closest_p1, closest_p2, min_dist
+    return arrPoints1, arrPoints2, closest_p1, closest_p2, min_dist
 
 def random_point():
     points = []
@@ -29,6 +36,25 @@ def random_point():
         points.append(point)
     return points
 
+# plot 3D
+# input: array of points
+def visual(points):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[0], points[1], points[2], c='r', marker='o')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
+
 points = random_point()
-closest_p1, closest_p2, min_dist = closest_points(points)
-print("Titik terdekat: ", closest_p1, "dan", closest_p2, "dengan jarak", min_dist)
+arrPoints1, arrPoints2, closest_p1, closest_p2, min_dist = closest_points(points)
+
+for i in range(len(arrPoints1)):
+    print("Titik terdekat ", i+1, ": ", arrPoints1[i], "dan", arrPoints2[i])
+    print("dengan jarak", min_dist)
+
+#print("Titik terdekat : ", closest_p1, "dan", closest_p2)
+#print("dengan jarak", min_dist)
