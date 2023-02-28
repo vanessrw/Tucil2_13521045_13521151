89a2e9f9965c893b98598a94258c0df1eb0e0ffb
vanessrw <64979714+vanessrw@users.noreply.github.com>
2023-02-28 13:03:54 +0700

    Add files via upload

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -1,10 +1,16 @@
 import math
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sympy import true
 
 def jarak(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+
+def tuple_pair_points(arrpoint1, arrpoint2):
+    pairs = [(arrpoint1[i], arrpoint2[i]) for i in range (len(arrPoints1))]
+    return pairs
 
 def closest_points(points):
     n = len(points)
@@ -21,7 +27,10 @@ def closest_points(points):
                 closest_p1, closest_p2 = points[i], points[j]
                 arrPoints1.append(closest_p1)
                 arrPoints2.append(closest_p2)
-
+            elif dist == min_dist:
+                closest_p1, closest_p2 = points[i], points[j]
+                arrPoints1.append(closest_p1)
+                arrPoints2.append(closest_p2)  
     
     return arrPoints1, arrPoints2, closest_p1, closest_p2, min_dist
 
@@ -49,12 +58,55 @@ def visual(points):
 
     plt.show()
 
-# points = random_point()
-# arrPoints1, arrPoints2, closest_p1, closest_p2, min_dist = closest_points(points)
-
-# for i in range(len(arrPoints1)):
-#     print("Titik terdekat ", i+1, ": ", arrPoints1[i], "dan", arrPoints2[i])
-#     print("dengan jarak", min_dist)
-
 #print("Titik terdekat : ", closest_p1, "dan", closest_p2)
 #print("dengan jarak", min_dist)
+
+points = random_point()
+arrPoints1, arrPoints2, closest_p1, closest_p2, min_dist = closest_points(points)
+
+for i in range(len(arrPoints1)):
+    print("Titik terdekat ", i+1, ": ", arrPoints1[i], "dan", arrPoints2[i])
+    print("dengan jarak", min_dist)
+
+def isClose(point):
+    arrPointsFin = np.concatenate((arrPoints1, arrPoints2))
+    is_in_array = np.isin(points, arrPointsFin).all(axis=1).any()
+    return is_in_array
+
+def visualization(points):
+    # Extract x, y, and z coordinates from the array of points
+    x_coords = [point[0] for point in points]
+    y_coords = [point[1] for point in points]
+    z_coords = [point[2] for point in points]
+
+    # Define the indices of the points to differentiate
+    indices_to_differentiate = []
+    if isClose(points):
+        indices_to_differentiate.append(points)
+
+    # Create a 3D scatter plot of the points
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x_coords, y_coords, z_coords)
+
+    #coloring
+    #if isClose(points):
+    #    ax.scatter(x_coords, y_coords, z_coords, color='red')
+    if not isClose(points):
+        ax.scatter(x_coords, y_coords, z_coords, color='black')
+    if isClose(points):
+        ax.scatter(x_coords, y_coords, z_coords, color='red')
+
+    # Set the title and axis labels
+    ax.set_title("Scatter Plot of {} Random Points in 3D".format(len(points)))
+    ax.set_xlabel("X Coordinates")
+    ax.set_ylabel("Y Coordinates")
+    ax.set_zlabel("Z Coordinates")
+
+    # Display the plot
+    plt.show()
+
+
+print(tuple_pair_points(arrPoints1, arrPoints2))
+visualization(points)
+#visualization_target(arrPoints1, arrPoints2)
